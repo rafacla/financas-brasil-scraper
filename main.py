959eee56dd325c247308fca31ac4131d8b6a2749
rafacla <rafacla@github.com>
2022-11-11 08:23:12 -0300

    removing the prefix /api/ of the urls

--- main.py
+++ main.py
@@ -24,19 +24,19 @@ logger = logging.getLogger(__name__)  # the __name__ resolve to "main" since we 
 app = FastAPI()
 
 
-@app.post("/api/fundos", response_model=DescricaoFundoResponse, status_code=status.HTTP_201_CREATED)
+@app.post("/fundos", response_model=DescricaoFundoResponse, status_code=status.HTTP_201_CREATED)
 def create(request: DescricaoFundoRequest, db: Session = Depends(get_db)):
     fundo = DescricaoFundoRepository.save(db, DescricaoFundo(**request.dict()))
     return DescricaoFundoResponse.from_orm(fundo)
 
 
-@app.get("/api/fundos", response_model=list[DescricaoFundoResponse])
+@app.get("/fundos", response_model=list[DescricaoFundoResponse])
 def find_all(db: Session = Depends(get_db)):
     fundos = DescricaoFundoRepository.find_all(db)
     return [DescricaoFundoResponse.from_orm(fundo) for fundo in fundos]
 
 
-@app.get("/api/fundos/{cnpj}", response_model=DescricaoFundoResponse)
+@app.get("/fundos/{cnpj}", response_model=DescricaoFundoResponse)
 def find_by_id(cnpj: str, db: Session = Depends(get_db)):
     cnpj = cnpj.replace('.', '').replace('-', '').replace('/', '')
     cnpj_like = ''
@@ -52,7 +52,7 @@ def find_by_id(cnpj: str, db: Session = Depends(get_db)):
     return DescricaoFundoResponse.from_orm(fundo)
 
 
-@app.delete("/api/fundos/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/fundos/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_by_id(id: int, db: Session = Depends(get_db)):
     if not DescricaoFundoRepository.exists_by_id(db, id):
         raise HTTPException(
@@ -62,7 +62,7 @@ def delete_by_id(id: int, db: Session = Depends(get_db)):
     return DescricaoFundoResponse(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/api/fundos/{id}", response_model=DescricaoFundoResponse)
+@app.put("/fundos/{id}", response_model=DescricaoFundoResponse)
 def update(id: int, request: DescricaoFundoRequest, db: Session = Depends(get_db)):
     if not DescricaoFundoRepository.exists_by_id(db, id):
         raise HTTPException(
@@ -72,9 +72,9 @@ def update(id: int, request: DescricaoFundoRequest, db: Session = Depends(get_db
     return DescricaoFundoResponse.from_orm(fundo)
 
 
-@app.get("/api/cotas/{cnpj}", response_model=list[CotasFundoResponse])
-@app.get("/api/cotas/{cnpj}/{data_de}", response_model=list[CotasFundoResponse])
-@app.get("/api/cotas/{cnpj}/{data_de}/{data_ate}", response_model=list[CotasFundoResponse])
+@app.get("/cotas/{cnpj}", response_model=list[CotasFundoResponse])
+@app.get("/cotas/{cnpj}/{data_de}", response_model=list[CotasFundoResponse])
+@app.get("/cotas/{cnpj}/{data_de}/{data_ate}", response_model=list[CotasFundoResponse])
 def cotas_by_cnpj(cnpj: str, data_de=None, data_ate=None, db: Session = Depends(get_db)):
     cnpj = cnpj.replace('.', '').replace('-', '').replace('/', '')
     cnpj_like = ''
