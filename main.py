ba88a013321f5f1964339234805eabb6e2f3fac7
rafacla <rafacla@github.com>
2022-11-27 15:25:17 -0300

    changing port to 12001

--- main.py
+++ main.py
@@ -134,4 +134,4 @@ def tesouros_disponiveis(db: Session = Depends(get_db)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=12001)
