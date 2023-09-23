8ecfb6acd1b5e365ceec438a0385c2484266e2c2
Rafael Claudio <rafacla@github.com>
2023-09-23 14:50:36 -0300

    criando indice na tabela de cotas e removendo pontos e hifens para economizar espaço

--- database/models.py
+++ database/models.py
@@ -7,25 +7,25 @@ from database.database import Base
 
 
 class DescricaoFundo(Base):
-    __tablename__ = parameters.description_table_name
+    __tablename__ = 'fundos_cvm_descricao'
 
     id: int = Column(Integer, primary_key=True, index=True)
-    CNPJ_FUNDO: str = Column(String(18), nullable=False, unique=True)
+    CNPJ_FUNDO: str = Column(String(14), nullable=False, unique=True, index=True)
     DT_COMPTC: datetime.date = Column(Date, nullable=False)
     DENOM_SOCIAL: str = Column(String(150), nullable=False)
     NM_FANTASIA: str = Column(String(150), nullable=False)
 
 
 class CotasFundo(Base):
-    __tablename__ = parameters.quotes_table_name
+    __tablename__ = 'fundos_cvm_cotas'
 
     id: int = Column(Integer, primary_key=True, index=True)
-    CNPJ_FUNDO: str = Column(String(18), nullable=False, unique=False)
+    CNPJ_FUNDO: str = Column(String(14), nullable=False, unique=False, index=True)
     DT_COMPTC: datetime.date = Column(Date, nullable=False)
     VL_QUOTA: float = Column(Float, nullable=False)
 
 class TaxaDI(Base):
-    __tablename__ = parameters.taxa_di_table_name
+    __tablename__ = 'di_cetip'
 
     id: int = Column(Integer, primary_key=True, index=True)
     dataDI: datetime.date = Column(Date, nullable=False, unique=True)
@@ -34,7 +34,7 @@ class TaxaDI(Base):
 
 
 class Tesouro(Base):
-    __tablename__ = parameters.tesouro_direto_table_name
+    __tablename__ = 'tesouro_direto'
 
     id: int = Column(Integer, primary_key=True, index=True)
     nome: str = Column(String(18), nullable=False)
@@ -47,21 +47,21 @@ class Tesouro(Base):
     pu_base: float = Column(Float, nullable=False)
 
 class Scrapy_Fundos_Cotas(Base):
-    __tablename__ = parameters.scrapy_quotes_table_name
+    __tablename__ = 'scrapy_fundos_cvm_cotas'
 
     id: int = Column(Integer, primary_key=True, index=True)
     link: str = Column(String(50), nullable=False, unique=True)
     ultima_atualizacao: datetime.date = Column(Date, nullable=False)
 
 class Scrapy_Fundos_Descricao(Base):
-    __tablename__ = parameters.scrapy_description_table_name
+    __tablename__ = 'scrapy_fundos_cvm_descricao'
 
     id: int = Column(Integer, primary_key=True, index=True)
     link: str = Column(String(50), nullable=False, unique=True)
     ultima_atualizacao: datetime.date = Column(Date, nullable=False)
 
 class Scrapy_Tesouro_Direto(Base):
-    __tablename__ = parameters.scrapy_tesouro_direto_table_name
+    __tablename__ = 'scrapy_tesouro_direto'
 
     id: int = Column(Integer, primary_key=True, index=True)
     link: str = Column(String(50), nullable=False, unique=True)
