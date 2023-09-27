ae292be287617a487e62fe99b14d528fb42fda62
Rafael Claudio <rafacla@github.com>
2023-09-27 17:59:25 -0300

    closes #4 - finished reviewing sqlite indexes

--- database/models.py
+++ database/models.py
@@ -18,7 +18,7 @@ class CotasFundo(Base):
     __tablename__ = 'fundos_cvm_cotas'
     __table_args__ = (UniqueConstraint("CNPJ_FUNDO", "DT_COMPTC", name="_CNPJ_FUNDO_DT_COMPTC_UC"), )
 
-    id: int = Column(Integer, primary_key=True, index=True)
+    id: int = Column(Integer, primary_key=True)
     CNPJ_FUNDO: int = Column(Integer, nullable=False, unique=False, index=True)
     DT_COMPTC: datetime.date = Column(Date, nullable=False)
     VL_QUOTA: float = Column(Float, nullable=False)
@@ -27,7 +27,7 @@ class CotasFundo(Base):
 class TaxaDI(Base):
     __tablename__ = 'di_cetip'
 
-    id: int = Column(Integer, primary_key=True, index=True)
+    id: int = Column(Integer, primary_key=True)
     dataDI: datetime.date = Column(Date, nullable=False, unique=True)
     taxaDIAnual: float = Column(Float, nullable=False)
     taxaDIDiaria: float = Column(Float, nullable=False)
@@ -36,7 +36,7 @@ class TaxaDI(Base):
 class Tesouro(Base):
     __tablename__ = 'tesouro_direto'
 
-    id: int = Column(Integer, primary_key=True, index=True)
+    id: int = Column(Integer, primary_key=True)
     nome: str = Column(String(18), nullable=False)
     vencimento: datetime.date = Column(Date, nullable=False)
     data: datetime.date = Column(Date, nullable=False)
@@ -49,20 +49,20 @@ class Tesouro(Base):
 class Scrapy_Fundos_Cotas(Base):
     __tablename__ = 'scrapy_fundos_cvm_cotas'
 
-    id: int = Column(Integer, primary_key=True, index=True)
+    id: int = Column(Integer, primary_key=True)
     link: str = Column(String(50), nullable=False, unique=True)
     ultima_atualizacao: datetime.date = Column(Date, nullable=False)
 
 class Scrapy_Fundos_Descricao(Base):
     __tablename__ = 'scrapy_fundos_cvm_descricao'
 
-    id: int = Column(Integer, primary_key=True, index=True)
+    id: int = Column(Integer, primary_key=True)
     link: str = Column(String(50), nullable=False, unique=True)
     ultima_atualizacao: datetime.date = Column(Date, nullable=False)
 
 class Scrapy_Tesouro_Direto(Base):
     __tablename__ = 'scrapy_tesouro_direto'
 
-    id: int = Column(Integer, primary_key=True, index=True)
+    id: int = Column(Integer, primary_key=True)
     link: str = Column(String(50), nullable=False, unique=True)
     ultima_atualizacao: datetime.date = Column(Date, nullable=False)
