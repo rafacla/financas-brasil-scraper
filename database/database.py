32d95ec249f0417a228038147e065ea58faff9a9
Rafael Claudio <rafacla@github.com>
2023-10-15 16:09:17 -0300

    fix database creation issues

--- database/database.py
+++ database/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -7,9 +7,9 @@ import parameters as parameters
 engine = create_engine(
     'sqlite:///' + parameters.database + '.db'
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
